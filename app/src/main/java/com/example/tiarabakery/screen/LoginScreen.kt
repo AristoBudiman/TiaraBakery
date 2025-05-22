bdd242e96bc7d464cc93043dde36bd01e00e01c0
AristoBudiman <aristobudimannew@gmail.com>
2025-05-22 21:43:19 +0700

    add Signup and Login Screen

--- app/src/main/java/com/example/tiarabakery/screen/LoginScreen.kt
+++ app/src/main/java/com/example/tiarabakery/screen/LoginScreen.kt
@@ -1,9 +1,122 @@
 package com.example.tiarabakery.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tiarabakery.R
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
+    var email by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFF1DE))
+            .padding(horizontal = 24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.tiara_vector),
+            contentDescription = "LogoTiara",
+            modifier = Modifier.fillMaxWidth()
+                .height(300.dp)
+        )
+
+        Text(
+            text = "Welcome back",
+            style = TextStyle(
+                fontSize = 25.sp,
+                fontFamily = FontFamily(Font(R.font.catamaran_medium)),
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Center
+            )
+        )
+
+        Text(
+            text = "Login to your account",
+            style = TextStyle(
+                fontSize = 25.sp,
+                fontFamily = FontFamily(Font(R.font.catamaran_medium)),
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Center
+            )
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email address") },
+            modifier = Modifier.fillMaxWidth(),
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Enter your password ...") },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = {
+
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+        ) {
+            Text(
+                text = "Login",
+                fontSize = 22.sp,
+                fontFamily = FontFamily(Font(R.font.catamaran_medium))
+            )
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+
+        TextButton(onClick = {
+
+        }) {
+            Text(text = "Don't have an account, Signup")
+        }
+
+
+
+    }
 }
