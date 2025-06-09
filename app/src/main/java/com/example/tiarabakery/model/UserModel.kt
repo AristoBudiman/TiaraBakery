13f7862438057f72b9b4112d23070327a22c5cfb
AvecenaHidayat <avecenahidayat@student.uns.ac.id>
2025-06-09 11:04:59 +0700

    update checkout page

--- app/src/main/java/com/example/tiarabakery/model/UserModel.kt
+++ app/src/main/java/com/example/tiarabakery/model/UserModel.kt
@@ -1,8 +1,11 @@
 package com.example.tiarabakery.model
 
+import okhttp3.Address
+
 data class UserModel(
     val name: String="",
     val email: String="",
     val uid: String="",
-    val cartItems : Map<String, Long> = emptyMap()
+    val cartItems : Map<String, Long> = emptyMap(),
+    val address: String = ""
 )
--- app/src/main/java/com/example/tiarabakery/pages/CheckoutPages.kt
+++ app/src/main/java/com/example/tiarabakery/pages/CheckoutPages.kt
@@ -1,8 +1,15 @@
 package com.example.tiarabakery.pages
 
+import android.icu.text.CaseMap.Title
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -11,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tiarabakery.AppUtil
@@ -44,6 +52,10 @@ fun Checkoutpage(modifier: Modifier = Modifier){
         mutableStateOf(0f)
     }
 
+    val total = remember {
+        mutableStateOf(0f)
+    }
+
     fun calculateAndAssign(){
         productList.forEach {
             if (it.actualPrice.isNotEmpty()) {
@@ -54,6 +66,7 @@ fun Checkoutpage(modifier: Modifier = Modifier){
 
         discount.value = subTotal.value * (AppUtil.getDiscountPercentage())/100;
         tax.value = subTotal.value * (AppUtil.getTaxPercentage())/100
+        total.value = "%.2f".format(subTotal.value - discount.value + tax.value).toFloat()
     }
 
     LaunchedEffect(key1 = Unit) {
@@ -86,6 +99,43 @@ fun Checkoutpage(modifier: Modifier = Modifier){
             .padding(16.dp)
     ){
         Text(text = "Checkout", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Deliver to : ", fontWeight = FontWeight.SemiBold)
+        Text(text = userModel.value.name)
+        Text(text = userModel.value.address)
+        Spacer(modifier = Modifier.height(16.dp))
+        HorizontalDivider()
+        Spacer(modifier = Modifier.height(16.dp))
+        RowCheckoutItems(title = "Subtotal", value = subTotal.value.toString())
+        Spacer(modifier = Modifier.height(16.dp))
+        RowCheckoutItems(title = "Discount (-)", value = discount.value.toString())
+        Spacer(modifier = Modifier.height(16.dp))
+        RowCheckoutItems(title = "Tax (+)", value = tax.value.toString())
+        HorizontalDivider()
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "To Pay",
+            textAlign = TextAlign.Center
+        )
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Rp"+total.value.toString(),
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
     }
 
+}
+
+@Composable
+fun RowCheckoutItems(title: String,value: String){
+    Row (
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        Text(text = title, fontSize = 20.sp, fontWeight = FontWeight.SemiBold)
+        Text(text = "Rp"+value, fontSize = 18.sp)
+    }
 }
